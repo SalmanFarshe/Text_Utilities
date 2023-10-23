40bf2383a27d58597f54b88896ab237cdb11251c
Salman Farshe <salman.zihad@involve.ai>
2023-10-23 14:29:43 +0600

    color theme modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import "./App.css";
+import About from "./Components/About";
+import LogIN from "./Components/LogIN";
 import NavBar from "./Components/NavBar";
 import TextForm from "./Components/TextForm";
-import LogIN from "./Components/LogIN";
-import About from "./Components/About";
-import { useState } from "react";
 function App() {
-  const [colorTheme, setcolorTheme] = useState('dark')
+  const [defaultBackgroundColorTheme, setdefaultBackgroundColorTheme] =
+    useState("black");
   const changeTheme = () => {
-    if (colorTheme === 'dark') {
-      setcolorTheme('white')
-    }
-    else {
-      setcolorTheme('dark')
-    }
+    setdefaultBackgroundColorTheme("#00000095");
+    document.body.style.backgroundColor = "#000000dd";
+  };
+  const changeThemeone = () => {
+    setdefaultBackgroundColorTheme("#007bff95");
+    document.body.style.backgroundColor = "#007bff95";
+  };
+  const changeThemetwo = () => {
+    setdefaultBackgroundColorTheme("#6C75aa95");
+    document.body.style.backgroundColor = "#6C75aa95";
+  };
+  const changeThemethree = () => {
+    setdefaultBackgroundColorTheme("#28a74595");
+    document.body.style.backgroundColor = "#28a74595";
+  };
+  const changeThemefour = () => {
+    setdefaultBackgroundColorTheme("#6f42c195");
+    document.body.style.backgroundColor = "#6f42c195";
+  };
+  const changeThemefive = () => {
+    setdefaultBackgroundColorTheme("#ffc10795");
+    document.body.style.backgroundColor = "#ffc10795";
   };
   return (
     <>
@@ -22,11 +39,16 @@ function App() {
         itemTwo="About"
         itemThree="Join Us"
         themeFunc={changeTheme}
-        themecolor={colorTheme}
+        themeFunc1={changeThemeone}
+        themeFunc2={changeThemetwo}
+        themeFunc3={changeThemethree}
+        themeFunc4={changeThemefour}
+        themeFunc5={changeThemefive}
+        themeColorBackground={defaultBackgroundColorTheme}
       />
       <About />
-      <LogIN />
-      <TextForm />
+      <LogIN themeColorBackground={defaultBackgroundColorTheme} />
+      <TextForm themeColorBackground={defaultBackgroundColorTheme} />
     </>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,16 +1,28 @@
 import React from "react";
 export default function NavBar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg bg-${props.themecolor}`}>
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{
+        backgroundColor: props.themeColorBackground,
+      }}
+    >
       <div className="container">
-        <a className="navbar-brand text-info" href="\">
+        <a
+          className={`navbar-brand text-${
+            props.themeColorBackground === "black" ? "white" : "white"
+          }`}
+          href="\"
+        >
           <b>{props.brandName}</b>
         </a>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a
-                className="nav-link active text-info"
+                className={`nav-link active text-${
+                  props.themeColorBackground === "black" ? "white" : "white"
+                }`}
                 aria-current="page"
                 href="\"
               >
@@ -18,24 +30,65 @@ export default function NavBar(props) {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-info" href="\">
+              <a
+                className={`nav-link text-${
+                  props.themeColorBackground === "black" ? "white" : "white"
+                }`}
+                href="\"
+              >
                 {props.itemTwo}
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-info" href="\">
+              <a
+                className={`nav-link text-${
+                  props.themeColorBackground === "black" ? "white" : "white"
+                }`}
+                href="\"
+              >
                 {props.itemThree}
               </a>
             </li>
           </ul>
-          <div className="form-check form-switch">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckChecked"
+          <div className="">
+            <div
+              className="rounded bg-dark p-1 mx-2 text-white"
               onClick={props.themeFunc}
-            />
+            >
+              Select Theme
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-2">
+              <div
+                className="rounded bg-primary p-3 text-white"
+                onClick={props.themeFunc1}
+              ></div>
+            </div>
+            <div className="col-md-2">
+              <div
+                className="rounded bg-secondary p-3 text-white"
+                onClick={props.themeFunc2}
+              ></div>
+            </div>
+            <div className="col-md-2">
+              <div
+                className="rounded bg-success p-3 text-white"
+                onClick={props.themeFunc3}
+              ></div>
+            </div>
+            <div className="col-md-2">
+              <div
+                className="rounded bg-info p-3 text-white"
+                onClick={props.themeFunc4}
+              ></div>
+            </div>
+            <div className="col-md-2">
+              <div
+                className="rounded bg-warning p-3 text-dark"
+                onClick={props.themeFunc5}
+              ></div>
+            </div>
           </div>
         </div>
       </div>
