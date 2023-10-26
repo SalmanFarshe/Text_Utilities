87f3d315c2eab2e4349f407bfa18c54b740543d9
Salman Farshe <salman.zihad@involve.ai>
2023-10-26 16:38:54 +0600

    Dynamic Title Added

--- src/App.js
+++ src/App.js
@@ -15,26 +15,32 @@ function App() {
   const changeTheme = () => {
     setdefaultBackgroundColorTheme("#00000095");
     bg = document.body.style.backgroundColor = "#000000dd";
+    document.title = "Text Utiles - Solid Dark mode";
   };
   const changeThemeone = () => {
     setdefaultBackgroundColorTheme("#007bff95");
     bg = document.body.style.backgroundColor = "#007bff95";
+    document.title = "Text Utiles - Solid Blue mode";
   };
   const changeThemetwo = () => {
     setdefaultBackgroundColorTheme("#6C75aa95");
     bg = document.body.style.backgroundColor = "#6C75aa95";
+    document.title = "Text Utiles - Solid Gray mode";
   };
   const changeThemethree = () => {
     setdefaultBackgroundColorTheme("#28a74595");
     bg = document.body.style.backgroundColor = "#28a74595";
+    document.title = "Text Utiles - Solid Green mode";
   };
   const changeThemefour = () => {
     setdefaultBackgroundColorTheme("#6f42c195");
     bg = document.body.style.backgroundColor = "#6f42c195";
+    document.title = "Text Utiles - Solid Purple mode";
   };
   const changeThemefive = () => {
     setdefaultBackgroundColorTheme("#ffc10795");
     bg = document.body.style.backgroundColor = "#ffc10795";
+    document.title = "Text Utiles - Solid Gold mode";
   };
   document.body.style.backgroundColor = bg;
   
